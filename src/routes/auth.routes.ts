a3536c67f03edd81051dbdee40044c964ee6af2e
guihsil <guilherme1234oliveira.go@gmail.com>
2026-06-11 12:59:49 -0300

    adding new attr

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -14,7 +14,4 @@ authRouter.post("/cadastro", cadastrar);
 // POST ─ /auth/login — autentica e retorna JWT
 authRouter.post("/login", logarUsuario);
 
-// GET ─ /auth/me — rota protegida: retorna dados do usuário logado
-authRouter.get("/me", authMiddleware, perfil);
-
 export default authRouter;
